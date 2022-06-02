2c52495790a249d515bcba1658e8c126266a7ded
gabrrielsilva <gabrielsilvaev@gmail.com>
2022-06-02 10:19:52 -0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const __dirname = path.dirname(__filename);
 
 let drawing = new Drawing();
 
-fs.createReadStream(path.resolve(__dirname, '..', 'public', 'parse.csv'))
+const dxfFile = path.resolve(__dirname, '..', 'export', 'output.dxf');
+const csvFile = path.resolve(__dirname, '..', 'public', 'parse.csv')
+
+fs.createReadStream(csvFile)
   .pipe(csv.parse({ headers: false }))
   .on('error', (error) => console.error(error))
   .on('data', async (row) => {
@@ -17,16 +20,17 @@ fs.createReadStream(path.resolve(__dirname, '..', 'public', 'parse.csv'))
            x = row[1],
            y = row[2];
 
-    draw(id, x, y);
+    draw(id, Number(x), Number(y));
   })
   .on('end', async () => {
     setTimeout(() => {
-      fs.writeFileSync(path.resolve(__dirname, '..', 'export', 'output.dxf'), drawing.toDxfString());
-    }, 1500)
+      fs.writeFileSync(dxfFile, drawing.toDxfString())
+    }, 2000)
   });
 
 function draw(id, x, y) {
   drawing.setUnits('Millimeters');
-  drawing.drawCircle(Number(x), Number(y), 0.0001);
-  drawing.drawText(Number(x) + 0.0003, Number(y), 0.00006, 0, id);
+  drawing.drawText(x, y, 0.00006, 0, id);
+  // drawing.drawText(x + 0.0002, y, 0.00006, 0, id);
+  // drawing.drawCircle(x, y, 0.0001);
 }
